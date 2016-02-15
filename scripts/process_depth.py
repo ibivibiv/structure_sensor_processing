67fe70f9519e7aab67e94eedaf750d272d3c728f
kscottz <katherineAScott@gmail.com>
2016-02-15 15:36:45 -0800

    this works, need to convert input from float to int with scaling, annoying but not hard.

--- scripts/process_depth.py
+++ scripts/process_depth.py
@@ -17,15 +17,12 @@ class ProcessDepth:
         self.bridge = CvBridge() 
 
         # Allow our topics to be dynamic.
-        self.input_camera_topic = rospy.get_param('~input_camera_topic', '/camera/image_rect_color')
-        self.output_camera_topic = rospy.get_param('~output_camera_topic', '/camera/color_stuff')
+        self.input_camera_topic = rospy.get_param('~input_camera_topic', '/camera/depth/image_rect')
+        self.output_camera_topic = rospy.get_param('~output_camera_topic', '/processed')
 
         # WE are going to publish a debug image as it comes in.
         self.pub = rospy.Publisher(self.output_camera_topic, Image,queue_size=10)
-        # We also publish the position of the thing we found.
-        self.point_pub = rospy.Publisher("/position",Point,queue_size=10)
-        # we get our input from a topic, it is of type image, call _show_colored_stuff.
-        rospy.Subscriber(self.input_camera_topic, Image, self._show_colored_stuff)
+        rospy.Subscriber(self.input_camera_topic, Image, self._process_depth_img)
         # run the node
         self._run()
 
@@ -33,28 +30,23 @@ class ProcessDepth:
     def _run(self):
         rospy.spin()
             
-    def _show_colored_stuff(self,input):
+    def _process_depth_img(self,input):
         # convert our image to CV2 numpy format from ROS format
         latest_image = self.bridge.imgmsg_to_cv2(input)
+
         if( latest_image is not None ):
             try:
                 # convert the image to SimpleCV
-                img = scv.Image(latest_image, cv2image=True, colorSpace=scv.ColorSpace.RGB)
-                # create a writeable copy
-                src = img.copy()
-                # Do our image processing get hue, threshold, morphology, connected components.
-                mask = src.hueDistance(180).invert().threshold(240).erode(2).dilate(3)
-                blobs = src.findBlobsFromMask(mask=mask)
-                # if we find something 
-                if( blobs ):
-                    # draw its convex hull with alpha
-                    blobs[-1].drawHull(width=-1,color=scv.Color.YELLOW,alpha=128)
-                    # and publish a position update
-                    pt = Point(x=blobs[-1].x,y=blobs[-1].y,z=blobs[-1].area())
-                    self.point_pub.publish(pt)
-                    src = src.applyLayers()
+                dmin = np.nanmin(latest_image)
+                dmax = np.nanmax(latest_image)
+                latest_image = latest_image - dmin
+                sf = 255.0/(dmax-dmin)
+                latest_image = sf*latest_image
+                temp = latest_image.astype(np.uint8)
+                img = scv.Image(temp, cv2image=True, colorSpace=scv.ColorSpace.RGB)
+                img = img.threshold(128)
                 # convert SimpleCV to CV2 Numpy Format
-                cv2img = src.getNumpyCv2()
+                cv2img = img.getNumpyCv2()
                 # Convert Cv2 numpy to ROS format
                 img_msg = self.bridge.cv2_to_imgmsg(cv2img, "bgr8")
                 # publish the topic.
